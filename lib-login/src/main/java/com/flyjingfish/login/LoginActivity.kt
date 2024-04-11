e8e204892f95791215cdb7861f9e03daa2989459
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2024-04-11 19:12:42 +0800

    完善route

--- lib-login/src/main/java/com/flyjingfish/login/LoginActivity.kt
+++ lib-login/src/main/java/com/flyjingfish/login/LoginActivity.kt
@@ -3,10 +3,12 @@ package com.flyjingfish.login
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.flyjingfish.login.databinding.ActivityLoginBinding
 import com.flyjingfish.module_communication_annotation.ImplementClassUtils
 import com.flyjingfish.user.UserHelper
 import com.flyjingfish.user.`User_UserActivity$$Router`
+import com.flyjingfish.user.`User_UserFragment$$Router`
 
 class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +19,11 @@ class LoginActivity: AppCompatActivity() {
         val user = userHelper.getUser()
         binding.btnGo.setOnClickListener {
             `User_UserActivity$$Router`.goUser_UserActivity(this,"hahah",user)
+        }
+
+        binding.btnGoFragment.setOnClickListener {
+            val fragment : Fragment = `User_UserFragment$$Router`.getUser_UserFragment("lalala",user) as Fragment
+            supportFragmentManager.beginTransaction().replace(R.id.container,fragment).commit()
         }
 
         Log.e("user",""+user)
---
+++ lib-user/src/main/java/com/flyjingfish/user/UserFragment.kt
@@ -0,0 +1,34 @@
+package com.flyjingfish.user
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.flyjingfish.module_communication_annotation.Route
+import com.flyjingfish.module_communication_annotation.RouteParams
+import com.flyjingfish.user.databinding.ActivityUserBinding
+
+@Route("user/UserFragment")
+class UserFragment : Fragment() {
+    @delegate:RouteParams("params1")
+    val params1 :String ? by lazy(LazyThreadSafetyMode.NONE) {
+        arguments?.getString("params1")
+    }
+
+    @delegate:RouteParams("params2")
+    val params2 :User ? by lazy(LazyThreadSafetyMode.NONE) {
+        arguments?.getSerializable("params2") as User
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = ActivityUserBinding.inflate(inflater,container,false)
+        Log.e("UserFragment","params1=$params1,params2=$params2")
+        return binding.root
+    }
+
+}
