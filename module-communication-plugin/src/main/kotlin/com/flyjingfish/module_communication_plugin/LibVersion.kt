15017bc687b02867f75325876749f2963e908a0c
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2024-05-31 17:34:43 +0800

    升级版本

--- module-communication-plugin/src/main/kotlin/com/flyjingfish/module_communication_plugin/LibVersion.kt
+++ module-communication-plugin/src/main/kotlin/com/flyjingfish/module_communication_plugin/LibVersion.kt
@@ -1,7 +1,7 @@
 package com.flyjingfish.module_communication_plugin
 
 object LibVersion {
-    const val version = "1.2.3"
+    const val version = "1.2.4"
     const val buildDir = "communication"
     const val pathName = "java"
     const val resName = "res"
